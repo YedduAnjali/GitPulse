acda169b0181bd5f0a091857f222b6eff64a3ab1
Yeddu Anjali <anjali41718@gmail.com>
2026-05-17 14:25:41 +0530

    Add root health route

--- server/src/server.js
+++ server/src/server.js
@@ -11,3 +11,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("GitPulse API is running successfully 🚀");
+});
